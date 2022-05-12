5c806d930ad340c24cf61fcd45f7128c6bdad124
Randel Hu <huxiayu0115@gmail.com>
2022-05-11 22:27:36 -0700

    add more pages and js

--- JS/home.js
+++ JS/home.js
@@ -1,2 +1,39 @@
-const build=document.querySelector('#buildResume');
-console.log(build);
+function getUserID() {
+  return [0, 0];
+}
+
+function signIn() {
+  const accounts = [
+    ["myaccount", "mypassword1"],
+    ["myaccount2", "mypassword2"],
+  ];
+  const id = document.getElementById("accountId").value;
+  const password = document.getElementById("password").value;
+  currentUser = getUserID();
+  let accountSize = accounts.length - 1;
+  let logInFail = true;
+  while (accountSize >= 0) {
+    if (
+      id === accounts[accountSize][0] &&
+      password === accounts[accountSize][1]
+    ) {
+      console.log("true");
+      signInForm.action = "builder.html";
+      logInFail = false;
+      console.log('sign in success');
+      break;
+    }
+    accountSize--;
+  }
+  if (logInFail) {
+    if (!document.getElementById("signInFail")) {
+      signInForm.innerHTML +=
+        "<br><br><p class='signInFail' id='signInFail'>Account ID/Password isn't correct, please enter again.</p>";
+      signInFail = true;
+    } else {
+      signInForm.innerHTML = signInForm.innerHTML;
+    }
+    console.log('sign in fail display');
+  }
+}
+const signInForm = document.getElementById("info");
